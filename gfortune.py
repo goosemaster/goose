b102b28fc0ae06fbcec4b89d45a6ba56ec0a0aeb
goosemaster <lanix9@gmail.com>
2016-09-07 16:20:45 -0500

    Create gfortune.py

---
+++ gfortune.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from BeautifulSoup import BeautifulSoup
+import urllib
+import urllib2
+
+
+def fortune():
+
+        a = "a"
+        for i in a:
+         try:
+
+                url= 'http://your-url-here.com/quote.php'
+                req = urllib2.Request(url, headers={'User-Agent' : "Goose Browser"})
+                page = urllib2.urlopen( req )
+                soup = BeautifulSoup(page.read())
+                quote =  soup.find("option").contents
+                return quote[0]
+         except Exception as e:
+                return('honk honk honk honk honk!?')
