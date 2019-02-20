8da1544cbf13ed6c08e142c9ceebd44e5056148f
Goose <lanix9@gmail.com>
2019-02-20 01:51:47 -0600

    cryptotest.py
    Test logic to build on in future function

---
+++ cryptotest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import pika
+from cryptography.fernet import Fernet
+
+MSG = "HERP DERP PLZ IGNORE DERPY DERP"
+MSGBYTES = str.encode(MSG)
+
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(MSGBYTES)
+token
+f = Fernet(key)
+DEC = f.decrypt(token)
+
+#print(DEC)
+
+RESULT = DEC.decode("utf-8")
+print(RESULT)
