77554e533af83a0cbb59a649421b9d0188481deb
Goose <lanix9@gmail.com>
2023-11-12 17:29:23 -0600

    Update gfortune.py

--- gfortune.py
+++ gfortune.py
@@ -1,28 +1,20 @@
-import aiml
-import os
-import sys
-import gfortune
-import subprocess
-from subprocess import call
+#!/usr/bin/env python3
 
+from BeautifulSoup import BeautifulSoup
+import urllib
+import urllib2
 
-def scmd(command):
+def fortune():
 
+        a = "a"
+        for i in a:
+         try:
 
-        testVar = "GGG"
-
-
-
-        if command == "quit":
-                return "HONK HONK HONK HONK HONK HONK HONK HONK"
-
-
-        elif command == "fortune":
-                output = gfortune.fortune()
-
-
-        else:
-                output = subprocess.check_output(command , shell =True)
-
-        response = str(output)
-        return response
+                url= 'http://your-url-here.com/quote.php'
+                req = urllib2.Request(url, headers={'User-Agent' : "Goose Browser"})
+                page = urllib2.urlopen( req )
+                soup = BeautifulSoup(page.read())
+                quote =  soup.find("option").contents
+                return quote[0]
+         except Exception as e:
+                return('honk honk honk honk honk!?')
